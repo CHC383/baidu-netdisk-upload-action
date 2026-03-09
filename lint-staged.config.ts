088db2a283b925e4e00405cfd1ac52674a48e327
Henry Cai <huacong.cai@gmail.com>
2026-03-09 15:40:43 -0700

    build(ci): add a pnpm script to run check_dist.sh and add renovate config

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -1,8 +1,8 @@
 export default {
-  "package.json": () => ["pnpm install", "./scripts/check_dist.sh"],
+  "package.json": () => ["pnpm install", "pnpm check-dist"],
   "src/**/*.ts": (stagedFiles: string[]) => [
     `eslint --fix --no-warn-ignored ${stagedFiles.join(" ")}`,
     `prettier --write --ignore-unknown ${stagedFiles.join(" ")}`,
-    "./scripts/check_dist.sh",
+    "pnpm check-dist",
   ],
 };
