14af38d3d9a338724950a11fb63d8439edfac07f
Henry Cai <huacong.cai@gmail.com>
2026-03-10 17:04:27 -0700

    chore(lint): remove printing diff details in check_dist.sh to speed up pre-commit check

--- scripts/check_dist.sh
+++ scripts/check_dist.sh
@@ -15,7 +15,6 @@ if [ ! -d dist/ ]; then
 fi
 
 if [ "$(git diff --ignore-space-at-eol --text dist/ | wc -l)" -gt "0" ]; then
-  echo "Detected uncommitted changes after build. See status below:"
-  git diff --ignore-space-at-eol --text dist/
+  echo "Detected uncommitted changes after build."
   exit 1
 fi
