56137d1b0b4da79ed47154b018767ee0d01493ee
Henry Cai <huacong.cai@gmail.com>
2026-06-12 13:04:41 -0700

    chore(lint): remove obsolete lint-stage config

--- lint-staged.config.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  "package.json": () => ["pnpm install", "pnpm check-dist"],
-  "src/**/*.ts": (stagedFiles: string[]) => [
-    `pnpm biome:check-fix ${stagedFiles.join(" ")}`,
-    "pnpm check-dist",
-  ],
-};
